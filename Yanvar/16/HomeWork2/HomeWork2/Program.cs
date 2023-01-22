4fce84d5089f6cfe958fe492f7b39f4ae5ede112
Royal Humbetov <humbatovroyaldev@gmail.com>
2023-01-22 21:46:58 +0400

    16 ev tapsirigi

---
+++ Yanvar/16/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Homework 1:
+             Ev tapşırığı olaraq ilk növbədə ən az 2-3 saat internetdə "C# data types" (C# veri tipleri) mövzusunu geniş bir şəkildə araşdırırsınız.
+             Araşdıra-araşdıra da bir Console App açıb araşdırdığınız data tiplərindən nümunələr yazırsınız. Hər data type üçün ən az 5 fərqli nümunə yazın.
+
+             Homework 2:
+             int, bool, char, string və long tipində dəyişənlər yaradıb onlara dəyər verirsiniz. Sonra elə bir dəyişən yaratmalısınız ki, bütün bu dəyişənləri
+             ona dəyər olaraq mənimsədə biləsiniz.
+
+             Homework 3:
+             Araşdırdığınız dəyişən tipləri barədə bir yazı hazırlayacaqsız. Hər bir data type'ın nə kimi bir dəyər aldığını, neçə bit olduğunu və s. araşdırın.
+
+             Qeyd: mövzuları araşdırarkən bir saytdan və ya qaynaqdan araşdırmayın. Fərqli-fərqli qaynaqlara baxın.
+            */
+
+            object red = 548445686;
+            object blue = false;
+            object black = '?';
+            object yellow = "World";
+            object green = 4856667979898524624;
+
+            Console.WriteLine(red);
+            Console.WriteLine(blue);
+            Console.WriteLine(black);
+            Console.WriteLine(yellow);
+            Console.WriteLine(green);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
