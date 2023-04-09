f4101219e3dec5db018297a4e720fb80b4f67181
Royal Humbetov <humbatovroyaldev@gmail.com>
2023-04-09 15:42:06 +0400

    Mart 6 Tapsirigi

---
+++ Mart/6 Mart/HomeWorks/HomeWorks/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+   public class Student : Person
+    {
+        public string ClassName { get; set; }
+        public string TeacherName { get; set; }
+
+
+        public override void ShowInfo()
+        {
+           base.ShowInfo();
+        }
+
+    }
+
+}
